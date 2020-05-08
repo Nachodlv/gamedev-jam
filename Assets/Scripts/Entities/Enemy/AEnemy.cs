c68cea43eeb38b0ad6f20c6df2a92d773e6ec04e
Ignacio de la Vega <ignacio.delavega@ing.austral.edu.ar>
2020-05-07 21:48:52 -0300

    Implement player dying

--- Assets/Scripts/Entities/Enemy/AEnemy.cs
+++ Assets/Scripts/Entities/Enemy/AEnemy.cs
@@ -6,31 +6,21 @@ using UnityEngine;
 
 namespace Enemy
 {
-	public class AEnemy: DamageReceiver, IHaveStats, IPausable
+	public class AEnemy: DamageReceiver, IHaveStats
 	{
 		[SerializeField] private Stats stats;
 		public Stats Stats => stats;
-
-
-		protected override void Awake()
-		{
-			base.Awake();
-		}
-
+		
 		protected override void DealDamage(float damage, bool instantKill)
 		{
 			stats.CurrentHealth = instantKill? 0 : stats.CurrentHealth - damage;
+			if(stats.CurrentHealth <= 0) Die();
 		}
+		
 
-		public void Pause()
+		private void Die()
 		{
-			Debug.Log($"{gameObject.name} I am paused!");
-		}
-
-		public void UnPause()
-		{
-			Debug.Log($"{gameObject.name} I am upaused!");
-
+			gameObject.SetActive(false);
 		}
 	}
 }
