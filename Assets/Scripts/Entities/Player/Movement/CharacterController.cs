391e654df0fc0fa4c5e3f1ade110c2ab14de516c
Ignacio de la Vega <ignacio.delavega@ing.austral.edu.ar>
2020-05-20 22:03:21 -0300

    Improve player jump

--- Assets/Scripts/Entities/Player/Movement/CharacterController.cs
+++ Assets/Scripts/Entities/Player/Movement/CharacterController.cs
@@ -7,17 +7,13 @@ namespace Entities.Player.Movement
 	public class CharacterController : MonoBehaviour
 	{
 		[SerializeField] private float jumpForce = 400f;							// Amount of force added when the player jumps.
-		[Range(0, 1)] [SerializeField] private float crouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
 		[Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;	// How much to smooth out the movement
-		[SerializeField] private bool airControl;							// Whether or not a player can steer while jumping;
 		[SerializeField] private LayerMask whatIsGround;							// A mask determining what is ground to the character
 		[SerializeField] private Transform groundCheck;							// A position marking where to check if the player is grounded.
-		[SerializeField] private Transform ceilingCheck;							// A position marking where to check for ceilings
-		[SerializeField] private Collider2D crouchDisableCollider;				// A collider that will be disabled when crouching
 		[SerializeField] private int maxJumps = 1;
-		[SerializeField] private SpriteRenderer spriteRenderer;
 		[SerializeField] private float maxVelocity = 5f;
 		[SerializeField] private float timeJumping = 1f;
+		[SerializeField] private float airControl = 100f;
 	
 		public event Action OnJumpEvent;
 		public event Action OnLandEvent;
@@ -27,14 +23,9 @@ namespace Entities.Player.Movement
 		public Vector3 Velocity => _myRigidBody2D.velocity;
 		public bool FacingRight { get; private set; } = true;
 
-		public bool AirControl
-		{
-			get => airControl;
-			set => airControl = value;
-		}
+		public bool AirControl { get; set; } = true;
 
 		private const float GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-		private const float CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
 
 		private Rigidbody2D _myRigidBody2D;
 		private Vector3 _velocity = Vector3.zero;
@@ -58,7 +49,6 @@ namespace Entities.Player.Movement
 				Jump();
 				return;
 			}			
-			// if(Grounded) return;
 			var wasGrounded = Grounded;
 
 			var size = Physics2D.OverlapCircleNonAlloc(groundCheck.position, GroundedRadius, _colliders, whatIsGround);
@@ -80,69 +70,30 @@ namespace Entities.Player.Movement
 	
 		public void Move(float move, bool crouch, bool canFlip )
 		{
-			// If crouching, check to see if the character can stand up
-			if (!crouch)
-			{
-				// If the character has a ceiling preventing them from standing up, keep them crouching
-				if (Physics2D.OverlapCircle(ceilingCheck.position, CeilingRadius, whatIsGround))
-				{
-					crouch = true;
-				}
-			}
-
-			//only control the player if grounded or airControl is turned on
-			if (Grounded || airControl)
-			{
-
-				// If crouching
-				if (crouch)
-				{
-					if (!_wasCrouching)
-					{
-						_wasCrouching = true;
-						OnCrouchEvent?.Invoke(true);
-					}
-
-					// Reduce the speed by the crouchSpeed multiplier
-					move *= crouchSpeed;
-
-					// Disable one of the colliders when crouching
-					if (crouchDisableCollider != null)
-						crouchDisableCollider.enabled = false;
-				} else
-				{
-					// Enable the collider when not crouching
-					if (crouchDisableCollider != null)
-						crouchDisableCollider.enabled = true;
-
-					if (_wasCrouching)
-					{
-						_wasCrouching = false;
-						OnCrouchEvent?.Invoke(false);
-					}
-				}
-
-				// Move the character by finding the target velocity
-				var velocity = _myRigidBody2D.velocity;
+		
+			// Move the character by finding the target velocity
+			var velocity = _myRigidBody2D.velocity;
+			// if (Grounded)
+			// {
 				Vector3 targetVelocity = new Vector2(move * 10f, velocity.y);
-				// And then smoothing it out and applying it to the character
-				var newVelocity = Vector3.SmoothDamp(velocity, targetVelocity, ref _velocity, movementSmoothing);
-				newVelocity.y = Mathf.Min(maxVelocity, newVelocity.y);
-				newVelocity.x = Mathf.Clamp(newVelocity.x, -maxVelocity, maxVelocity);
+				var newVelocity = Vector3.SmoothDamp(velocity, targetVelocity, ref _velocity, 
+					movementSmoothing, maxVelocity);
 				_myRigidBody2D.velocity = newVelocity;
-
-				// If the input is moving the player right and the player is facing left...
-				if (move > 0 && !FacingRight && canFlip)
-				{
-					// ... flip the player.
-					Flip();
-				}
-				// Otherwise if the input is moving the player left and the player is facing right...
-				else if (move < 0 && FacingRight && canFlip)
-				{
-					// ... flip the player.
-					Flip();
-				}
+			// }
+			// else if(AirControl && Mathf.Abs(_myRigidBody2D.velocity.x) < maxVelocity)
+			// {
+			// 	Debug.Log("In The Air");
+			// 	Vector3 targetVelocity = new Vector2(move * airControl, 0);
+			// 	_myRigidBody2D.AddForce(targetVelocity);
+			// }
+		
+			if (move > 0 && !FacingRight && canFlip)
+			{
+				Flip();
+			}
+			else if (move < 0 && FacingRight && canFlip)
+			{
+				Flip();
 			}
 		
 		}
