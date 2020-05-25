4c3697b7052fea02f6a364c63938d1a14ba2e6d8
Ignacio de la Vega <ignacio.delavega@ing.austral.edu.ar>
2020-05-24 23:19:13 -0300

    Add first tutorial level

---
+++ Assets/Scripts/UI/TextShower.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using Entities.Player;
+using UnityEngine;
+using Utils;
+
+namespace UI
+{
+    public class TextShower : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private float timeShowing = 1f;
+        [SerializeField] private float fadeSpeed = 1f;
+        [SerializeField] private Collider2D collider2D;
+
+        private Func<IEnumerator> _fadeIn;
+        private Func<IEnumerator> _fadeOut;
+        private WaitSeconds _waitSeconds;
+        private bool showed;
+
+        private void Awake()
+        {
+            _fadeIn = FadeIn;
+            _fadeOut = FadeOut;
+            canvasGroup.alpha = 0;
+            _waitSeconds = new WaitSeconds(this, () => StartCoroutine(_fadeOut()), timeShowing);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var player = other.GetComponentInChildren<APlayer>();
+            if (player == null) return;
+            ShowText();
+            collider2D.enabled = false;
+        }
+
+        private void ShowText()
+        {
+            StartCoroutine(_fadeIn());
+            _waitSeconds.Wait();
+        }
+
+        private IEnumerator FadeIn()
+        {
+            while (canvasGroup.alpha < 0.99f)
+            {
+                canvasGroup.alpha += Time.deltaTime * fadeSpeed;
+                yield return null;
+            }
+        }
+
+        private IEnumerator FadeOut()
+        {
+            while (canvasGroup.alpha > 0.01f)
+            {
+                canvasGroup.alpha -= Time.deltaTime * fadeSpeed;
+                yield return null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/CoroutineQueue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// <para>Runs multiple coroutines one after the other</para>
+/// </summary>
+public class CoroutineQueue
+{
+    private readonly Queue<IEnumerator> _coroutines;
+    private bool _executingCoroutines;
+    private readonly MonoBehaviour _monoBehaviour;
+    private readonly Func<IEnumerator> _executeCoroutines;
+
+    public CoroutineQueue(MonoBehaviour monoBehaviour, int initialQuantity)
+    {
+        _monoBehaviour = monoBehaviour;
+        _coroutines = new Queue<IEnumerator>(initialQuantity);
+        _executeCoroutines = ExecuteCoroutines;
+    }
+
+    /// <summary>
+    /// <para>Adds a new coroutine to the queue. If no coroutine is executing, it will start executing the
+    /// coroutine passed as parameter</para>
+    /// </summary>
+    /// <param name="coroutine"></param>
+    public void AddCoroutine(IEnumerator coroutine)
+    {
+        _coroutines.Enqueue(coroutine);
+        if(_executingCoroutines) return;
+        _executingCoroutines = true;
+        _monoBehaviour.StartCoroutine(_executeCoroutines());
+    }
+
+    private IEnumerator ExecuteCoroutines()
+    {
+        while (_coroutines.Count > 0)
+        {
+            yield return _monoBehaviour.StartCoroutine(_coroutines.Peek());
+            _coroutines.Dequeue();
+        }
+
+        _executingCoroutines = false;
+    }
+}
